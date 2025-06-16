058ca1829049d33973ceb9d92240937215f03e69
Was01 <80862278+Was01@users.noreply.github.com>
2025-06-16 20:57:06 +0100

    Create aula06.cpp

---
+++ aula06/aula06.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+
+#define d_num 50
+
+using namespace std;
+
+
+
+int main(){
+	
+	int v_num=10;
+	const int c_num=-4;
+	cout<<v_num<<endl;
+	cout<<c_num<<endl;
+	cout<<d_num<<endl;
+	return 0;
+	
+}
