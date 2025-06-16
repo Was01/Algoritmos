9a6d80f2868ccf1e7ed66d6def85bde751a84fd9
Was01 <80862278+Was01@users.noreply.github.com>
2025-06-16 23:06:10 +0100

    Create aula07.cpp

---
+++ aula07/aula07.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int num;
+	
+	cout<< "Digite um numero inteiro: ";
+	
+	cin >> num;
+	
+	cout<< "Numero digitado: " <<num<<endl;
+	
+	return 0;
+}
